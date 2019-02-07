fd7bacfc5598ba14d9d1417c6b65a898b6d7b64f
beardofdoom <marcin.jusiak@gmail.com>
2019-02-07 23:01:09 +0100

    Add controlled components to AddItem component

--- src/components/AddItem/AddItem.js
+++ src/components/AddItem/AddItem.js
@@ -3,6 +3,36 @@ import './AddItem.css';
 
 class AddItem extends Component {
 
+    minNumber = 1;
+
+    state = {
+        textInput: '',
+        numberInput: this.minNumber,
+        checkboxInput: false,
+    }
+
+    handleTextChange = (e) => {
+        this.setState({
+            textInput: e.target.value
+        })
+    }
+
+    handleNumberChange = (e) => {
+        this.setState({
+            numberInput: e.target.value,
+        })
+    }
+
+    handleCheckboxChange = (e) => {
+        this.setState({
+            checkboxInput: !this.state.checkboxInput
+        });
+    }
+
+    handleSubmitAddItem = (e) => {
+        e.preventDefault();
+        console.log('Added item');
+    }
 
 
     render() {
@@ -10,16 +40,22 @@ class AddItem extends Component {
         return (
             <>
                 <h2>Add new item</h2>
-                <form>
-                    <label htmlFor="name">Name
-                            <input type="text" />
+                <form onSubmit={this.handleSubmitAddItem}>
+                    <label htmlFor="name">
+                        Name
+                        <input type="text" value={this.state.textInput} onChange={this.handleTextChange} />
                     </label>
-                    <label htmlFor="quantity">Quantity
-                            <input type="number" min="1" />
+                    <label htmlFor="quantity">
+                        Quantity
+                        <input type="number" min={this.minNumber} value={this.state.numberInput} onChange={this.handleNumberChange} />
+                    </label>
+                    <label htmlFor="important">
+                        Important
+                        <input type="checkbox" checked={this.state.checkboxInput} onChange={this.handleCheckboxChange} />
                     </label>
                     <button>Add to list</button>
-                    <button>Cancel</button>
                 </form>
+                <button>Cancel</button>
             </>
 
         )
