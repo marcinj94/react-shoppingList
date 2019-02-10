f2c79c83934d721527d9e139928eda2f9d235243
beardofdoom <marcin.jusiak@gmail.com>
2019-02-10 21:57:37 +0100

    Updates

---
+++ precache-manifest.8a74e056eb9f052e8c04092c70181484.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8455e4296d9649cff42699a10faadb88",
+    "url": "/react-shoppingList/static/media/bg.8455e429.jpg"
+  },
+  {
+    "revision": "989195fff400e283f925",
+    "url": "/react-shoppingList/static/js/runtime~main.989195ff.js"
+  },
+  {
+    "revision": "0446bb1debf4aa8f55cb",
+    "url": "/react-shoppingList/static/js/main.0446bb1d.chunk.js"
+  },
+  {
+    "revision": "f6b84cc0c44b8101ab3f",
+    "url": "/react-shoppingList/static/js/1.f6b84cc0.chunk.js"
+  },
+  {
+    "revision": "0446bb1debf4aa8f55cb",
+    "url": "/react-shoppingList/static/css/main.de9e21a6.chunk.css"
+  },
+  {
+    "revision": "f75cac264924d63f0335ad1a6ec07e01",
+    "url": "/react-shoppingList/index.html"
+  }
+];
